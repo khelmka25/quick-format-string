d6b8dded60561e8970eb60e220454537a7eaed70
khelmka25 <kihelmka@gmail.com>
2025-11-10 16:47:44 -0600

    Add testing, add LICENSE, add equality operators, constructors, and assignment operators.

--- include/QFString.h
+++ include/QFString.h
@@ -3,17 +3,44 @@
 #include <string>
 #include <type_traits>
 
+// TODO: investigate using different char types:
+// char8_t, char16_t, char32_t
 template <std::size_t N, typename char_t = char>
 class QFString {
+public:
+    QFString() { clear(); }
+
+    QFString(const QFString& rhs) { copy(rhs); }
+
+    QFString(QFString&& rhs) { copy(rhs); }
+
+public:
+    QFString& operator=(const QFString& rhs) {
+        copy(rhs);
+        return *this;
+    }
+
+    QFString& operator=(QFString&& rhs) {
+        copy(rhs);
+        return *this;
+    }
+
+    bool operator==(const std::string& rhs) {
+        // Negative value if lhs appears before rhs in lexicographical order.
+        // Zero if lhs and rhs compare equal.
+        // Positive value if lhs appears after rhs in lexicographical order.
+        return std::strcmp(buffer, rhs.c_str()) == 0;
+    }
+
+    bool operator==(const QFString& rhs) { return strcmp(buffer, rhs.c_str()) == 0; }
+
+    bool operator==(const char_t* rhs) { return strcmp(buffer, rhs) == 0; }
+
 public:
     static constexpr std::size_t capacity() noexcept {
         static_assert(N >= 1, "capacity cannot be negative");
         // always include null term at end of string
         return (N - 1ull);
-    }
-
-    QFString() {
-        clear();
     }
 
     void clear() {
@@ -34,6 +61,10 @@ public:
         return ptr - buffer;
     }
 
+    bool empty() const noexcept {
+        return size() == 0ull;
+    }
+
     std::basic_string<char_t> string() const noexcept {
         // array decays to cstr_type
         // https://en.cppreference.com/w/cpp/string/basic_string/basic_string.html
@@ -41,23 +72,36 @@ public:
         return std::basic_string<char_t>(buffer, size());
     }
 
-    char_t* c_str() const noexcept { return buffer; }
+    const char_t* c_str() const noexcept { return buffer; }
 
     template <typename... Ti>
-    void format(const char_t* format, Ti&& ...ti) & noexcept {
+    void format(const char_t* fmt, Ti&&... ti) & noexcept {
         // int snprintf( char* restrict buffer, size_t bufsz,
         //           const char* restrict format, ... );
         // Writes the results to a character string buffer. At most bufsz - 1 characters are written. The resulting
         // character string will be terminated with a null character, unless bufsz is zero. If bufsz is zero, nothing is
         // written and buffer may be a null pointer, however the return value (number of bytes that would be written not
         // including the null terminator) is still calculated and returned.
-        std::snprintf(buffer, N, format, std::forward<Ti>(ti)...);
+        std::snprintf(buffer, N, fmt, std::forward<Ti>(ti)...);
     }
 
     template <typename... Ti>
-    QFString<N, char_t>&& format(const char_t* format, Ti&& ...ti) && noexcept {
-        std::snprintf(buffer, N, format, std::forward<Ti>(ti)...);
+    QFString<N, char_t>&& format(const char_t* fmt, Ti&&... ti) && noexcept {
+        std::snprintf(buffer, N, fmt, std::forward<Ti>(ti)...);
         return std::move(*this);
+    }
+
+private:
+    void copy(QFString&& rhs) noexcept {
+        for (auto i = 0ull; i < rhs.capacity(); i++) {
+            this->buffer[i] = rhs.buffer[i];
+        }
+    }
+
+    void copy(const QFString& rhs) noexcept {
+        for (auto i = 0ull; i < rhs.capacity(); i++) {
+            this->buffer[i] = rhs.buffer[i];
+        }
     }
 
 private:
--- tests/test_format.cc
+++ tests/test_format.cc
@@ -3,8 +3,56 @@
 
 #include "QFString.h"
 
-using QFString16 = QFString<16, char>;
+TEST_CASE("Comprehensive Test") {
+    {
+        // emptiness test
+        QFString<32> empty_str32;
+        REQUIRE(empty_str32.capacity() == 31ull);
+        REQUIRE(empty_str32.size() == 0ull);
+    }
 
-TEST_CASE("32 Character Formatting Test") {
-    REQUIRE(QFString<32>().format("%02i + %02i = %02i", 40, 2, 42).string() == std::string("40 + 02 = 42"));
+    {
+        QFString<32> str32;
+        str32.format("my name is... %s", "Andrew");
+        REQUIRE(str32.string() == std::string("my name is... Andrew"));
+
+        // copy constructor
+        auto str32_copy(str32);
+        REQUIRE(str32_copy.string() == std::string("my name is... Andrew"));
+
+        // move constructor (in this case, there is no moving, just copying (buffer is stack allocated))
+        auto str32_move(std::move(str32));
+        REQUIRE(str32_move.string() == std::string("my name is... Andrew"));
+    }
+
+    {
+        QFString<32> str32;
+        str32.format("The year is %d", 2025);
+        REQUIRE(str32.string() == std::string("The year is 2025"));
+
+        // copy assignment
+        auto str32_copy_asgn = str32;
+        REQUIRE(str32_copy_asgn.string() == std::string("The year is 2025"));
+
+        // move assignment
+        auto str32_move_asgn = std::move(str32);
+        REQUIRE(str32_move_asgn.string() == std::string("The year is 2025"));
+    }
+
+    {
+        // equality test
+        auto equality_test = QFString<32>().format("%02i + %02i = %02i", 40, 2, 42);
+
+        REQUIRE(equality_test == "40 + 02 = 42");
+        REQUIRE(equality_test == std::string("40 + 02 = 42"));
+        REQUIRE(equality_test == QFString<32>().format("%02i + %02i = %02i", 40, 2, 42));
+    }
+
+    {
+        // test that the buffer is not overflown (buffer is not overflown)
+        QFString<8> buffer_overflow_test;
+        buffer_overflow_test.format("%d + %d is %d", 1, 2, 3);
+        REQUIRE(buffer_overflow_test.size() < 8);
+        REQUIRE(buffer_overflow_test == "1 + 2 i");
+    }
 }
